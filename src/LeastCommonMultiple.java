df2de665b51e831c899397612b23b31b1859f8c5
austenhasty <hastyae@gmail.com>
2020-08-17 16:00:40 -0700

    More efficient solution incorporating gcd method to help find lcm

--- src/LeastCommonMultiple.java
+++ src/LeastCommonMultiple.java
@@ -4,17 +4,31 @@ public class LeastCommonMultiple {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        long a = s.nextInt();
-        long b = s.nextInt();
+        int a = s.nextInt();
+        int b = s.nextInt();
         System.out.println(leastCommonMultiple(a, b));
+//        System.out.println(greatestCommonDivisor(a, b));
     }
 
-    public static long leastCommonMultiple(long a, long b) {
+/*    public static long leastCommonMultiple(long a, long b) {
         for (long i = 1; i <= a*b; i++) {
             if (i%a == 0 && i%b == 0) {
                 return i;
             }
         }
         return a*b;
+    } */
+
+    public static long leastCommonMultiple(int a, int b) {
+        return (long) (a *(b / greatestCommonDivisor(a,b)));
+    }
+
+    public static long greatestCommonDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return greatestCommonDivisor(b, a%b);
+        }
+
     }
 }
